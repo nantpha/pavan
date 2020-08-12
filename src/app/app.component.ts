d1ef3da466ec17bc40fbcc585d9be1d8b50a54a0
Prashanth Thota <prashanth.thota@otsi.com>
2020-08-12 09:51:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'task';
+  isSubmitted = false;
+
+  // City Names
+  option: any = ['00001', '00002', '00003', '00004']
+
+  constructor(public fb: FormBuilder) { }
+
+  /*########### Form ###########*/
+  registrationForm = this.fb.group({
+    cityName: ['', [Validators.required]]
+  })
+
+
+  // Choose city using select dropdown
+  changeCity(e) {
+    console.log(e.value)
+    this.cityName.setValue(e.target.value, {
+      onlySelf: true
+    })
+  }
+
+  // Getter method to access formcontrols
+  get cityName() {
+    return this.registrationForm.get('cityName');
+  }
+
+  /*########### Template Driven Form ###########*/
+  onSubmit() {
+    this.isSubmitted = true;
+    if (!this.registrationForm.valid) {
+      return false;
+    } else {
+      alert(JSON.stringify(this.registrationForm.value))
+    }
+
+  }
 }
